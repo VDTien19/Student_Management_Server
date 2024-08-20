9c77e4df0ea7d1b2cb84e2daddfec3d5c54b8dfc
tiennehihi <vuductientienti@gmail.com>
2024-08-21 00:29:40 +0700

    require gvcn when create user

--- src/Model/User.model.js
+++ src/Model/User.model.js
@@ -7,7 +7,8 @@ const UserSchema = new Schema({
   },
   gvcn: {
     type: Schema.Types.ObjectId,
-    ref: 'Teacher'
+    ref: 'Teacher',
+    required: true
   },
   fullname: {
     type: String,
